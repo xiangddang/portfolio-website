b6b56008051f5ff4818ec93801eaa44aed5f5532
xiangddang <935725453@qq.com>
2024-05-05 15:02:42 +0800

    about me part finished

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
+import AboutSection from "./components/AboutSection";
 import NavBar from "./components/NavBar";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
     <NavBar />
       <div class="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
+        <AboutSection />
       </div>
     </main>
   );
