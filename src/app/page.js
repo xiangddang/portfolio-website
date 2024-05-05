f151fd72e68b41ced5b94de8b24b6e19f4f0da67
xiangddang <935725453@qq.com>
2024-05-05 16:06:13 +0800

    projects part finished

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,17 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 import AboutSection from "./components/AboutSection";
+import ProjectsSection from "./components/ProjectsSection";
 import NavBar from "./components/NavBar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
     <NavBar />
-      <div class="container mt-24 mx-auto px-12 py-4">
+      <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
         <AboutSection />
+        <ProjectsSection />
       </div>
     </main>
   );
