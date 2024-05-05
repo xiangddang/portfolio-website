5d4ee6f6e939a78a790a200f56b9c925fae41ec5
xiangddang <935725453@qq.com>
2024-05-05 17:55:14 +0800

    contant && footer

--- src/app/page.js
+++ src/app/page.js
@@ -2,17 +2,21 @@ import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 import AboutSection from "./components/AboutSection";
 import ProjectsSection from "./components/ProjectsSection";
+import EmailSection from "./components/EmailSection";
+import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
-    <NavBar />
+      <NavBar />
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
         <AboutSection />
         <ProjectsSection />
+        <EmailSection />
       </div>
+      <Footer />
     </main>
   );
 }
