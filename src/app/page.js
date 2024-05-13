122b573de876aebbdb3bad6706bce679f446fabb
xiangddang <935725453@qq.com>
2024-05-13 11:27:09 +0800

    add ghchart

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 import AboutSection from "./components/AboutSection";
 import ProjectsSection from "./components/ProjectsSection";
